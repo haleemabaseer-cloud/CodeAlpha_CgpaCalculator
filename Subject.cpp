ef61e3380e6211eee508515eb61ee1be7013f3de
haleemabaseer-cloud <301408685+haleemabaseer-cloud@users.noreply.github.com>
2026-07-15 14:44:09 +0500

    Refactor Subject constructor to use assignment

--- Subject.cpp
+++ Subject.cpp
@@ -1,4 +1,8 @@
 #include "Subject.h"
 
 Subject::Subject(std::string name, int cr, float gp) 
-    : subjectname(name), credit(cr), gradepoint(gp) {}
+{
+    subjectname = name;
+    credit = cr;
+    gradepoint = gp;
+}
